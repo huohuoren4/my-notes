29a0fdabcc30468b6cb17d36d3a32a5e542aa593
tom01 <674860357@qq.com>
2023-04-09 00:46:49 +0800

    增加gorm

--- scripts/test.sql
+++ scripts/test.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主机： mysql
+-- 生成日期： 2023-04-08 12:36:57
+-- 服务器版本： 5.7.34
+-- PHP 版本： 8.0.27
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -19,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `blog_group` (
-`id` int(11) NOT NULL,
-`group_name` varchar(20) NOT NULL DEFAULT '小学组'
+  `id` int(11) NOT NULL,
+  `group_name` varchar(20) NOT NULL DEFAULT '小学组'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户组';
 
 --
@@ -39,12 +48,12 @@ INSERT INTO `blog_group` (`id`, `group_name`) VALUES
 --
 
 CREATE TABLE `blog_user` (
-`id` int(11) NOT NULL,
-`username` varchar(50) NOT NULL,
-`age` tinyint(3) NOT NULL DEFAULT '20',
-`price` decimal(5,2) NOT NULL,
-`description` varchar(255) NOT NULL,
-`group_id` varchar(20) NOT NULL COMMENT '组ID'
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `age` tinyint(3) NOT NULL DEFAULT '20',
+  `price` decimal(5,2) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `group_id` int(11) NOT NULL COMMENT '组ID'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 --
@@ -52,12 +61,12 @@ CREATE TABLE `blog_user` (
 --
 
 INSERT INTO `blog_user` (`id`, `username`, `age`, `price`, `description`, `group_id`) VALUES
-(1, '小唐', 0, '12.30', '广东省深圳市松白路', '1'),
-(8, '小明', 13, '30.00', '美国洛杉矶', '1'),
-(9, '小章', 70, '89.00', '英国伦敦', '3'),
-(10, '王小名', 40, '45.00', '法国巴黎', '2'),
-(11, '艾伦', 1, '14.00', '古巴比伦', '2'),
-(12, '李冰', 36, '45.63', '中国上海', '1');
+(1, '小唐', 0, '12.30', '广东省深圳市松白路', 1),
+(8, '小明', 13, '30.00', '美国洛杉矶', 1),
+(9, '小章', 70, '89.00', '英国伦敦', 3),
+(10, '王小名', 40, '45.00', '法国巴黎', 2),
+(11, '艾伦', 1, '14.00', '古巴比伦', 2),
+(12, '李冰', 36, '45.63', '中国上海', 1);
 
 --
 -- 转储表的索引
@@ -67,15 +76,15 @@ INSERT INTO `blog_user` (`id`, `username`, `age`, `price`, `description`, `group
 -- 表的索引 `blog_group`
 --
 ALTER TABLE `blog_group`
-ADD PRIMARY KEY (`id`),
-ADD UNIQUE KEY `group_name` (`group_name`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `group_name` (`group_name`);
 
 --
 -- 表的索引 `blog_user`
 --
 ALTER TABLE `blog_user`
-ADD PRIMARY KEY (`id`),
-ADD UNIQUE KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- 在导出的表使用AUTO_INCREMENT
@@ -85,13 +94,13 @@ ADD UNIQUE KEY `username` (`username`);
 -- 使用表AUTO_INCREMENT `blog_group`
 --
 ALTER TABLE `blog_group`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- 使用表AUTO_INCREMENT `blog_user`
 --
 ALTER TABLE `blog_user`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
